fa336e823e06b0a631561a2c4c6e060b9bd77be8
chen xiao <1090021294@qq.com>
2019-08-29 10:54:27 +0800

    my version

--- read.py
+++ read.py
@@ -1,11 +1,13 @@
 data = []
 count = 0
+sum = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
+		length = len(data[count])
+		sum = int(length) + sum
 		count += 1
 		if count % 1000 == 0: # %用来求余数
-			print(len(data))
-print(data[0])
-print('--------------')
-print(data[1])
+			print(len(data))		
+print('档案读取完毕，总共有', len(data), '笔资料')
+print('留言平均长度为', sum/len(data))
